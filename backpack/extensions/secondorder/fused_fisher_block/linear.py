3030dd6a829c1e42c4fea2392044df38957d980f
nobody <63745715+2zki3@users.noreply.github.com>
2021-08-26 14:26:02 -0400

    add linear bias.

--- backpack/extensions/secondorder/fused_fisher_block/linear.py
+++ backpack/extensions/secondorder/fused_fisher_block/linear.py
@@ -8,7 +8,6 @@ from backpack.extensions.secondorder.fused_fisher_block.fused_fisher_block_base 
 class FusedFisherBlockLinear(FusedFisherBlockBaseModule):
     def __init__(self, damping=1.0):
         self.damping = damping
-        self.alpha = alpha
         super().__init__(derivatives=LinearDerivatives(), params=["bias", "weight"])
 
     def weight(self, ext, module, g_inp, g_out, backproped):
@@ -46,25 +45,30 @@ class FusedFisherBlockLinear(FusedFisherBlockBaseModule):
 
 
     def bias(self, ext, module, g_inp, g_out, backproped):
-        # TODO(bmu): manully backprop quantities in the extra b/w pass
+        """
+        y = wx + b
+        g_inp: tuple of [dl/db (avg) = sum of dl/dy over batch dim, dl/dx, dl/dw]
+        g_out: tuple of [dl/dy (individual, divided by batch size m)]
+        backproped:
+            * symmetric factorization of the hessian w.r.t. output, i.e. S in H = SS^T (scaled by 1/sqrt(m))
+            * S^{(i-1)} = J^TS^{(i)}
+            * jacobian of loss w.r.t. bias params = transposed jacobian of output w.r.t. bias params @ S = S
+        fuse by manully backproping quantities in the extra b/w pass
+        """
+        # derivative of loss w.r.t. bias parameters = derivatie of loss w.r.t. layer output
+        g = g_inp[0]
+        m = g_out[0].shape[0]
 
-        grad = module.bias.grad
-        n = g_out[0].shape[0]
-        g_out_sc = n * g_out[0]
+        J = backproped.squeeze()
 
         # compute vector jacobian product in optimization method
-        grad_prod = einsum("no,o->n", (g_out_sc, grad))
-        # grad_prod = 0
-        out = einsum("no,lo->nl", g_out_sc, g_out_sc)
-        # out = 0
+        Jg = einsum("mp,p->m", (J, g))
+        JTJ = einsum("mp,lp->ml", J, J)
+        JTJ_inv = inv(JTJ + self.damping * eye(m).to(g.device))
+        v = matmul(JTJ_inv, Jg.unsqueeze(1)).squeeze()
+        gv = einsum("m,mp->p", (v, J))
 
-        NGD_kernel = out / n
-        NGD_inv = inv(NGD_kernel + self.damping * eye(n).to(grad.device))
-        v = matmul(NGD_inv, grad_prod.unsqueeze(1)).squeeze()
-        gv = einsum("n,no->o", (v, g_out_sc))
-        gv = gv / n
-
-        update = (grad - gv)/self.damping
+        update = (g - gv) / self.damping
 
         return update
 
