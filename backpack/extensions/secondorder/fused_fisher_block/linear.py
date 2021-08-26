4d4d0418f0f9122d7e3adfecf0841e5dbba237e0
nobody <63745715+2zki3@users.noreply.github.com>
2021-08-26 15:23:01 -0400

    add linear weight.

--- backpack/extensions/secondorder/fused_fisher_block/linear.py
+++ backpack/extensions/secondorder/fused_fisher_block/linear.py
@@ -11,36 +11,42 @@ class FusedFisherBlockLinear(FusedFisherBlockBaseModule):
         super().__init__(derivatives=LinearDerivatives(), params=["bias", "weight"])
 
     def weight(self, ext, module, g_inp, g_out, backproped):
-        # TODO(bmu): manully backprop quantities in the extra b/w pass
+        """
+        y = wx + b
+        g_inp: tuple of [dl/db (avg) = sum of dl/dy over batch dim, dl/dx, dl/dw]
+        g_out: tuple of [dl/dy (individual, divided by batch size m)]
+        backproped:
+            * symmetric factorization of the hessian w.r.t. output, i.e. S in H = SS^T (scaled by 1/sqrt(m))
+            * S^{(i-1)} = J^TS^{(i)}
+            * jacobian of loss w.r.t. weight params = transposed jacobian of output w.r.t. weight params @ S
+        fuse by manully backproping quantities in the extra b/w pass
+        """
+        # derivative of loss w.r.t. weight parameters = transposed derivatie of loss w.r.t. layer output @ I
+        m = g_out[0].shape[0]
 
         I = module.input0
-        n = g_out[0].shape[0]
-        g_out_sc = n * g_out[0]
-        G = g_out_sc
-        grad = module.weight.grad
+        G = backproped.squeeze() # scaled by 1/sqrt(m)
+        g = g_inp[2] # g = dw = einsum("mo,mi->io", (g_out[0], I))
 
-        B =  einsum("ni,li->nl", (I, I))
-        A =  einsum("no,lo->nl", (G, G))
+        # compute the covariance factors II and GG
+        II =  einsum("mi,li->ml", (I, I))
+        GG =  einsum("mo,lo->ml", (G, G))
 
-        # compute vector jacobian product in optimization method
-        grad_prod = einsum("ni,oi->no", (I, grad))
-        grad_prod = einsum("no,no->n", (grad_prod, G))
-        # grad_prod = 0
-        out = A * B
-        # out = 0
-        NGD_kernel = out / n
-        NGD_inv = inv(NGD_kernel + self.damping * eye(n).to(grad.device))
-        v = matmul(NGD_inv, grad_prod.unsqueeze(1)).squeeze()
+        # ngd update = J^T @ inv(JJ^T + damping * I) @ Jg
+        Jg = einsum("mi,io->mo", (I, g))
+        Jg = einsum("mo,mo->m", (Jg, G))
+        JJT = II * GG
+        JJT_inv = inv(JJT + self.damping * eye(m).to(g.device))
+        v = matmul(JJT_inv, Jg.unsqueeze(1)).squeeze()
+        gv = einsum("m,mo->mo", (v, G))
+        gv = einsum("mo,mi->oi", (gv, I))
 
-        gv = einsum("n,no->no", (v, G))
-        gv = einsum("no,ni->oi", (gv, I))
-        gv = gv / n
-
-        update = (grad - gv)/self.damping
+        update = (g.t() - gv) / self.damping
 
         module.I = I
         module.G = G
-        module.NGD_inv = NGD_inv
+        module.NGD_inv = JJT_inv
+
         return update
 
 
@@ -55,17 +61,17 @@ class FusedFisherBlockLinear(FusedFisherBlockBaseModule):
             * jacobian of loss w.r.t. bias params = transposed jacobian of output w.r.t. bias params @ S = S
         fuse by manully backproping quantities in the extra b/w pass
         """
-        # derivative of loss w.r.t. bias parameters = derivatie of loss w.r.t. layer output
+        # derivative of loss w.r.t. bias parameters = derivatie of loss w.r.t. layer output, i.e. J = G
         g = g_inp[0]
         m = g_out[0].shape[0]
 
         J = backproped.squeeze()
 
-        # compute vector jacobian product in optimization method
+        # ngd update = J^T @ inv(JJ^T + damping * I) @ Jg
         Jg = einsum("mp,p->m", (J, g))
-        JTJ = einsum("mp,lp->ml", J, J)
-        JTJ_inv = inv(JTJ + self.damping * eye(m).to(g.device))
-        v = matmul(JTJ_inv, Jg.unsqueeze(1)).squeeze()
+        JJT = einsum("mp,lp->ml", J, J)
+        JJT_inv = inv(JTJ + self.damping * eye(m).to(g.device))
+        v = matmul(JJT_inv, Jg.unsqueeze(1)).squeeze()
         gv = einsum("m,mp->p", (v, J))
 
         update = (g - gv) / self.damping
