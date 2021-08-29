c4c84f0f25c4a6652c03484ce4c344ec6546d6c6
nobody <63745715+2zki3@users.noreply.github.com>
2021-08-29 04:00:32 -0400

    backprop H_inv, J of f w.r.t. y, shape (m, c).

--- backpack/extensions/secondorder/fused_fisher_block/losses.py
+++ backpack/extensions/secondorder/fused_fisher_block/losses.py
@@ -1,5 +1,8 @@
 from functools import partial
 
+from torch.linalg import inv
+from torch import einsum, eye
+
 from backpack.core.derivatives.crossentropyloss import CrossEntropyLossDerivatives
 from backpack.extensions.secondorder.fused_fisher_block.fused_fisher_block_base import FusedFisherBlockBaseModule
 
@@ -9,8 +12,12 @@ class FusedFisherBlockLoss(FusedFisherBlockBaseModule):
         # backprop symmetric factorization of the hessian of loss w.r.t. the network outputs,
         # i.e. S in H = SS^T
         hess_func = self.make_loss_hessian_func(ext)
+        sqrt_H = hess_func(module, grad_inp, grad_out)
+        c_, m, c = sqrt_H.size()
+        H = einsum('omc,olv->cmvl', (sqrt_H, sqrt_H)).reshape(c * m, c * m)
+        H_inv = inv(H)
 
-        return hess_func(module, grad_inp, grad_out)
+        return (H_inv, eye(c, c * m).to(H_inv.device), (m, c))
 
     def make_loss_hessian_func(self, ext):
         # TODO(bmu): try both exact and MC sampling
