d34c5a51dbd424160eb0979b10801919a38d6a75
nobody <63745715+2zki3@users.noreply.github.com>
2021-08-29 06:38:50 -0400

    customize "backpropagate()".

--- backpack/extensions/secondorder/fused_fisher_block/fused_fisher_block_base.py
+++ backpack/extensions/secondorder/fused_fisher_block/fused_fisher_block_base.py
@@ -1,6 +1,11 @@
-from backpack.extensions.mat_to_mat_jac_base import MatToJacMat
+from backpack.extensions.module_extension import ModuleExtension
 
 
-class FusedFisherBlockBaseModule(MatToJacMat):
+class FusedFisherBlockBaseModule(ModuleExtension):
     def __init__(self, derivatives, params=None):
-        super().__init__(derivatives, params=params)
+        super().__init__(params=params)
+        self.derivatives = derivatives
+
+    def backpropagate(self, ext, module, g_inp, g_out, backproped):
+        H_inv, J, (m, c) = backproped
+        return [H_inv, self.derivatives.jac_t_mat_prod(module, g_inp, g_out, J), (m, c)]
